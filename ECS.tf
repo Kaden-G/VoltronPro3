2b076593e7974304c779078710eec120df0b0d73
codifer2point0 <154541492+codifer2point0@users.noreply.github.com>
2024-02-02 07:12:03 -0600

    MVP ECS.tf
    MVP

--- ECS.tf
+++ ECS.tf
@@ -1,4 +1,6 @@
+# AWS Provider Configuration
 provider "aws" {
+  alias  = "main"
   region = "us-east-1"  # Change to your desired region
 }
 
@@ -26,12 +28,14 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_attachment" {
 
 # ECS Cluster
 resource "aws_ecs_cluster" "my_cluster" {
-  name = "voltronecs"
+  provider = aws.main  # Specify the provider alias
+  name     = "voltronecs"
 }
 
 # ECS Task Definition
 resource "aws_ecs_task_definition" "my_task_definition" {
-  family                   = "my-task-family"
+  provider = aws.main  # Specify the provider alias
+  family                   = "web-app"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -51,13 +55,14 @@ resource "aws_ecs_task_definition" "my_task_definition" {
 
 # ECS Service
 resource "aws_ecs_service" "my_service" {
-  name            = "voltronecs"
+  provider = aws.main  # Specify the provider alias
+  name            = "Voltron_ECS"
   cluster         = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.my_task_definition.arn
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = ["subnet-0057664ba21bd4735"]  # Replace with your subnet IDs
-    security_groups = ["sg-0d1ccb3f21d0b03d9"]  # Replace with your security group IDs
+    subnets         = ["subnet-0057664ba21bd4735"]  # Replace with your subnet IDs
+    security_groups = ["sg-0d1ccb3f21d0b03d9"]      # Replace with your security group IDs
   }
 }
